96bed92149e4f6018956fab9bdd316e69b2cbb76
Lucas Pietan <148373645+lpietan@users.noreply.github.com>
2024-10-28 23:30:31 -0500

    Update rscripts.py

--- GenoMLizer/rscripts.py
+++ GenoMLizer/rscripts.py
@@ -2,18 +2,25 @@
 
 import subprocess
 import sys
+import importlib.resources as pkg_resources
+import GenoMLizer
 
 def CMI_main():
-    subprocess.run(['Rscript', 'GenoMLizer/CMI.R'] + sys.argv[1:], check=True)
+    with pkg_resources.path(GenoMLizer, 'CMI.R') as cmi_r_path:
+    subprocess.run(['Rscript', str(cmi_r_path)] + sys.argv[1:], check=True)
 
 def GLM_main():
-    subprocess.run(['Rscript', 'GenoMLizer/GLM.R'] + sys.argv[1:], check=True)
+    with pkg_resources.path(GenoMLizer, 'GLM.R') as glm_r_path:
+    subprocess.run(['Rscript', str(glm_r_path)] + sys.argv[1:], check=True)
 
 def DTVI_main():
-    subprocess.run(['Rscript', 'GenoMLizer/DTVI.R'] + sys.argv[1:], check=True)
+    with pkg_resources.path(GenoMLizer, 'DTVI.R') as dtvi_r_path:
+    subprocess.run(['Rscript', str(dtvi_r_path)] + sys.argv[1:], check=True)
 
 def mlVar_main():
-    subprocess.run(['Rscript', 'GenoMLizer/mlVar.R'] + sys.argv[1:], check=True)
+    with pkg_resources.path(GenoMLizer, 'mlVar.R') as mlVar_r_path:
+    subprocess.run(['Rscript', str(mlVar_r_path)] + sys.argv[1:], check=True)
 
 def mlGene_main():
-    subprocess.run(['Rscript', 'GenoMLizer/mlGene.R'] + sys.argv[1:], check=True)
+    with pkg_resources.path(GenoMLizer, 'mlGene.R') as mlGene_r_path:
+    subprocess.run(['Rscript', str(mlGene_r_path)] + sys.argv[1:], check=True)
