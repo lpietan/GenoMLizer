afcff4b63c25581bd2f18b3802c53b7c09f3132b
Lucas Pietan <148373645+lpietan@users.noreply.github.com>
2024-11-06 13:29:43 -0600

    Update mlVar.R

--- GenoMLizer/mlVar.R
+++ GenoMLizer/mlVar.R
@@ -12,7 +12,7 @@ args <- commandArgs(trailingOnly = TRUE)
 library(doSNOW)
 registerDoSNOW(makeCluster(as.numeric(args[6])))
 
-settings(control = function() CVControl(seed = as.numeric(args[5])))
+settings(control = CVControl(seed = as.numeric(args[5])))
 
 ## Load train data
 d_train <- read_csv(args[1])
