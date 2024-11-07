baafd0a0c22f84a427743a07da7f35374e30a076
Lucas Pietan <148373645+lpietan@users.noreply.github.com>
2024-11-07 12:30:43 -0600

    Update rscripts.py

--- GenoMLizer/rscripts.py
+++ GenoMLizer/rscripts.py
@@ -2,25 +2,46 @@
 
 import subprocess
 import sys
-import importlib.resources as pkg_resources
+import psutil
 import GenoMLizer
 
+def get_default_ppsize():
+    """Calculate default ppsize based on system memory"""
+    memory_gb = psutil.virtual_memory().total / (1024**3)
+    if memory_gb <= 8:
+        return 100000
+    elif memory_gb <= 16:
+        return 200000
+    elif memory_gb <= 32:
+        return 300000
+    else:
+        return 500000
+
+def run_rscript(script_name, args, ppsize=None):
+    """Generic R script runner with configurable ppsize"""
+    cmd = ['Rscript']
+    if ppsize:
+        cmd.extend(['--max-ppsize', str(ppsize)])
+    cmd.extend([f'GenoMLizer/{script_name}'] + args)
+    subprocess.run(cmd, check=True)
+
 def CMI_main():
-    with pkg_resources.path(GenoMLizer, 'CMI.R') as cmi_r_path:
-        subprocess.run(['Rscript', str(cmi_r_path)] + sys.argv[1:], check=True)
+    # Get ppsize from environment variable or use default
+    ppsize = int(os.environ.get('GENOMLIZER_PPSIZE', get_default_ppsize()))
+    run_rscript('CMI.R', sys.argv[1:], ppsize=ppsize)
 
 def GLM_main():
-    with pkg_resources.path(GenoMLizer, 'GLM.R') as glm_r_path:
-        subprocess.run(['Rscript', str(glm_r_path)] + sys.argv[1:], check=True)
+    ppsize = int(os.environ.get('GENOMLIZER_PPSIZE', get_default_ppsize()))
+    run_rscript('GLM.R', sys.argv[1:], ppsize=ppsize)
 
 def DTVI_main():
-    with pkg_resources.path(GenoMLizer, 'DTVI.R') as dtvi_r_path:
-        subprocess.run(['Rscript', str(dtvi_r_path)] + sys.argv[1:], check=True)
+    ppsize = int(os.environ.get('GENOMLIZER_PPSIZE', get_default_ppsize()))
+    run_rscript('DTVI.R', sys.argv[1:], ppsize=ppsize)
 
 def mlVar_main():
-    with pkg_resources.path(GenoMLizer, 'mlVar.R') as mlVar_r_path:
-        subprocess.run(['Rscript', str(mlVar_r_path)] + sys.argv[1:], check=True)
+    ppsize = int(os.environ.get('GENOMLIZER_PPSIZE', get_default_ppsize()))
+    run_rscript('mlVar.R', sys.argv[1:], ppsize=ppsize)
 
 def mlGene_main():
-    with pkg_resources.path(GenoMLizer, 'mlGene.R') as mlGene_r_path:
-        subprocess.run(['Rscript', str(mlGene_r_path)] + sys.argv[1:], check=True)
+    ppsize = int(os.environ.get('GENOMLIZER_PPSIZE', get_default_ppsize()))
+    run_rscript('mlGene.R', sys.argv[1:], ppsize=ppsize)
